48869865a703812b2c9166873d15a72ef6f3e213
gusaul <muhauliya@gmail.com>
2017-04-12 22:32:41 +0700

    change basic structure

--- driver.go
+++ driver.go
@@ -23,26 +23,3 @@ func (e *DynaMock) ExpectGetItem() *GetItemExpectation {
 
 	return &e.GetItemExpect[len(e.GetItemExpect)-1]
 }
-
-func (e *MockDynamoDB) GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
-	expectLength := len(e.dynaMock.GetItemExpect)
-	if expectLength > 0 {
-		x := e.dynaMock.GetItemExpect[expectLength-1]
-
-		if x.table != nil {
-			if *x.table != *input.TableName {
-				return nil, fmt.Errorf("Expect table %s but found table %s", *x.table, *input.TableName)
-			}
-		}
-
-		if x.key != nil {
-			if !reflect.DeepEqual(x.key, input.Key) {
-				return nil, fmt.Errorf("Expect key %+v but found key %+v", x.key, input.Key)
-			}
-		}
-
-		return x.output, nil
-	}
-
-	return nil, fmt.Errorf("Get Item Expectation Not Found")
-}
---
+++ get_item.go
@@ -0,0 +1,45 @@
+package dynamock
+
+import (
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func (e *GetItemExpectation) ToTable(table string) *GetItemExpectation {
+	e.table = &table
+	return e
+}
+
+func (e *GetItemExpectation) WithKeys(keys map[string]*dynamodb.AttributeValue) *GetItemExpectation {
+	e.key = keys
+	return e
+}
+
+func (e *GetItemExpectation) ExpectReturns(res dynamodb.GetItemOutput) *GetItemExpectation {
+	e.output = &res
+	return e
+}
+
+func (e *MockDynamoDB) GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
+	if len(e.dynaMock.GetItemExpect) > 0 {
+		x := e.dynaMock.GetItemExpect[0] //get first element of expectation
+
+		if x.table != nil {
+			if *x.table != *input.TableName {
+				return nil, fmt.Errorf("Expect table %s but found table %s", *x.table, *input.TableName)
+			}
+		}
+
+		if x.key != nil {
+			if !reflect.DeepEqual(x.key, input.Key) {
+				return nil, fmt.Errorf("Expect key %+v but found key %+v", x.key, input.Key)
+			}
+		}
+
+		// delete first element of expectation
+		e.dynaMock.GetItemExpect = append(e.dynaMock.GetItemExpect[:0], e.dynaMock.GetItemExpect[1:]...)
+
+		return x.output, nil
+	}
+
+	return nil, fmt.Errorf("Get Item Expectation Not Found")
+}
