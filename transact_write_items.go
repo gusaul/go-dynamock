6e42fba1ec49358b9e4900a5f346357e92558692
sha5hwati <shashwatishradha5@gmail.com>
2019-07-11 15:59:14 -0600

    Adding table check

--- transact_write_items.go
+++ transact_write_items.go
@@ -5,6 +5,12 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
+// Table - method for set Table expectation
+func (e *TransactWriteItemsExpectation) Table(table string) *TransactWriteItemsExpectation {
+	e.table = &table
+	return e
+}
+
 // WillReturns - method for set desired result
 func (e *TransactWriteItemsExpectation) WillReturns(res dynamodb.TransactWriteItemsOutput) *TransactWriteItemsExpectation {
 	e.output = &res
@@ -14,6 +20,20 @@ func (e *TransactWriteItemsExpectation) WillReturns(res dynamodb.TransactWriteIt
 func (e *MockDynamoDB) TransactWriteItems(input *dynamodb.TransactWriteItemsInput) (*dynamodb.TransactWriteItemsOutput, error){
 	if len(e.dynaMock.TransactWriteItemsExpect) > 0 {
 		x := e.dynaMock.TransactWriteItemsExpect[0] //get first element of expectation
+
+		foundTable := false
+		if x.table != nil {
+			for _, item := range input.TransactItems {
+				if x.table == item.Update.TableName || x.table == item.Put.TableName ||
+					x.table == item.Delete.TableName {
+					foundTable = true
+				}
+			}
+
+			if foundTable == false {
+				return nil, fmt.Errorf("Expect table %s not found", *x.table)
+			}
+		}
 
 		// delete first element of expectation
 		e.dynaMock.TransactWriteItemsExpect = append(e.dynaMock.TransactWriteItemsExpect[:0],
