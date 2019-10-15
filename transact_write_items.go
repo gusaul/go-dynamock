22fc6cd4c9ecb0a310c718b6bc9d365ac6f432e7
sangdeug.kim <sangdeug.kim@samsung.com>
2019-10-15 13:13:54 +0900

    Add TransactWriteItemsWithContext
    This patch introduces TransactWriteItemsWithContext which has
    same body of TransactWriteItems

--- transact_write_items.go
+++ transact_write_items.go
@@ -2,6 +2,8 @@ package dynamock
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
@@ -19,6 +21,38 @@ func (e *TransactWriteItemsExpectation) WillReturns(res dynamodb.TransactWriteIt
 
 // TransactWriteItems - this func will be invoked when test running matching expectation with actual input
 func (e *MockDynamoDB) TransactWriteItems(input *dynamodb.TransactWriteItemsInput) (*dynamodb.TransactWriteItemsOutput, error){
+	if len(e.dynaMock.TransactWriteItemsExpect) > 0 {
+		x := e.dynaMock.TransactWriteItemsExpect[0] //get first element of expectation
+
+		foundTable := false
+
+		if x.table != nil {
+			for _, item := range input.TransactItems {
+				if (item.Update != nil && x.table == item.Update.TableName) ||
+					(item.Put != nil && x.table == item.Put.TableName) ||
+					(item.Delete != nil && x.table == item.Delete.TableName) {
+						foundTable = true
+				}
+			}
+
+			if foundTable == false {
+				return nil, fmt.Errorf("Expect table %s not found", *x.table)
+			}
+		}
+
+		// delete first element of expectation
+
+		e.dynaMock.TransactWriteItemsExpect = append(e.dynaMock.TransactWriteItemsExpect[:0],
+			e.dynaMock.TransactWriteItemsExpect[1:]...)
+
+		return x.output, nil
+	}
+
+	return nil, fmt.Errorf("Transact Write Items Table Expectation Not Found")
+}
+
+// TransactWriteItemsWithContext - this func will be invoked when test running matching expectation with actual input
+func (e *MockDynamoDB) TransactWriteItemsWithContext(ctx aws.Context, input *dynamodb.TransactWriteItemsInput, opt ...request.Option) (*dynamodb.TransactWriteItemsOutput, error){
 	if len(e.dynaMock.TransactWriteItemsExpect) > 0 {
 		x := e.dynaMock.TransactWriteItemsExpect[0] //get first element of expectation
 
