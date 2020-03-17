728dd05dc123751e9e8108f3cc6e4b3f322533c6
Omar Elgabry <omar.elgabry.93@gmail.com>
2020-03-17 18:13:42 -0400

    Add WithItems to TransactWriteItems

--- transact_write_items.go
+++ transact_write_items.go
@@ -2,12 +2,20 @@ package dynamock
 
 import (
 	"fmt"
+	"reflect"
+
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 // Table - method for set Table expectation
 func (e *TransactWriteItemsExpectation) Table(table string) *TransactWriteItemsExpectation {
 	e.table = &table
+	return e
+}
+
+// WithItems - method for set Items expectation
+func (e *TransactWriteItemsExpectation) WithItems(items []*dynamodb.TransactWriteItem) *TransactWriteItemsExpectation {
+	e.items = items
 	return e
 }
 
@@ -18,32 +26,36 @@ func (e *TransactWriteItemsExpectation) WillReturns(res dynamodb.TransactWriteIt
 }
 
 // TransactWriteItems - this func will be invoked when test running matching expectation with actual input
-func (e *MockDynamoDB) TransactWriteItems(input *dynamodb.TransactWriteItemsInput) (*dynamodb.TransactWriteItemsOutput, error){
+func (e *MockDynamoDB) TransactWriteItems(input *dynamodb.TransactWriteItemsInput) (*dynamodb.TransactWriteItemsOutput, error) {
 	if len(e.dynaMock.TransactWriteItemsExpect) > 0 {
-		x := e.dynaMock.TransactWriteItemsExpect[0] //get first element of expectation
+		curTransactWrite := e.dynaMock.TransactWriteItemsExpect[0] // get first element of expectation
 
-		foundTable := false
+		// compare lengths
+		if len(curTransactWrite.items) != len(input.TransactItems) {
+			return nil, fmt.Errorf("Expect items %+v but found items %+v", curTransactWrite.items, input.TransactItems)
+		}
 
-		if x.table != nil {
-			for _, item := range input.TransactItems {
-				if (item.Update != nil && x.table == item.Update.TableName) ||
-					(item.Put != nil && x.table == item.Put.TableName) ||
-					(item.Delete != nil && x.table == item.Delete.TableName) {
-						foundTable = true
+		for i, item := range input.TransactItems {
+			// comapre table name for each write transact item with `x.table`
+			if curTransactWrite.table != nil {
+				if (item.Update != nil && curTransactWrite.table != item.Update.TableName) ||
+					(item.Put != nil && curTransactWrite.table != item.Put.TableName) ||
+					(item.Delete != nil && curTransactWrite.table != item.Delete.TableName) {
+					return nil, fmt.Errorf("Expect table %s not found", *curTransactWrite.table)
 				}
 			}
 
-			if foundTable == false {
-				return nil, fmt.Errorf("Expect table %s not found", *x.table)
+			// compare transact item - each item also contains the table name
+			if !reflect.DeepEqual(curTransactWrite.items[i], item) {
+				return nil, fmt.Errorf("Expect item %+v at index %d but found item %+v", curTransactWrite.items[i], i, item)
 			}
 		}
 
 		// delete first element of expectation
-
 		e.dynaMock.TransactWriteItemsExpect = append(e.dynaMock.TransactWriteItemsExpect[:0],
 			e.dynaMock.TransactWriteItemsExpect[1:]...)
 
-		return x.output, nil
+		return curTransactWrite.output, nil
 	}
 
 	return nil, fmt.Errorf("Transact Write Items Table Expectation Not Found")
